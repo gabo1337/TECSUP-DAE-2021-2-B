05b46808c313161d670cc872260c1f4a0c81e1b0
gabriel <gabriel.cordova@tecsup.edu.pe>
2021-09-02 20:16:27 -0500

    lab03

---
+++ lab03/encuesta/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'encuesta'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('<int:pregunta_id>/',views.detalle,name='detalle'),
+    path('<int:pregunta_id>/votar',views.votar,name='votar'),
+    path('<int:pregunta_id>/resultados/',views.resultados,name='resultados')
+
+]
