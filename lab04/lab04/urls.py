ea51d1f4bf41cc3a42ec51780c489f8b4da022b1
gabriel <gabriel.cordova@tecsup.edu.pe>
2021-09-09 08:59:59 -0500

    lab04

--- lab04/lab04/urls.py
+++ lab04/lab04/urls.py
@@ -1,4 +1,4 @@
-"""lab03 URL Configuration
+"""lab04 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('encuesta/', include('encuesta.urls')),
 ]
