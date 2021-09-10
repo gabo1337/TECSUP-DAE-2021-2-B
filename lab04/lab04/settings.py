9082d06ffe692d17abe3628afff7ce94bb4143b3
gabriel <gabriel.cordova@tecsup.edu.pe>
2021-09-09 20:20:41 -0500

    lab04

--- lab04/lab04/settings.py
+++ lab04/lab04/settings.py
@@ -71,13 +71,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'lab04.wsgi.application'
 
 
-# Database
+# Databasez
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- lab04/recetas/models.py
+++ lab04/recetas/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+class Receta(models.Model):
+    titulo = models.CharField(max_length=100, unique=True)
+    ingredientes = models.TextField(help_text='Redacta los intgredientes')
+    preparacion = models.TextField()
+    tiempo_registrado = models.DateTimeField(auto_now=True)
+    autor = models.ForeignKey(Autor,on_delete=CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    receta = models.ForeignKey(Receta,on_delete=CASCADE)
+    texto = models.TextField(help_text=u'tu comentario',verbose_name='Comentario')
+
+    def __str__(self):
+        return self.texto
