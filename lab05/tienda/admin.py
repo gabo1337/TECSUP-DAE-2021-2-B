f3b09367b19a13a585526abc6c34fcc083b1ccbf
gabriel <gabriel.cordova@tecsup.edu.pe>
2021-09-23 08:16:40 -0500

    lab06

--- lab05/tienda/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Categoria,Producto,Cliente
-
-admin.site.register(Categoria)
-admin.site.register(Cliente)
-admin.site.register(Producto)
