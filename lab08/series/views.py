4235e6d701aa821e8dd0136119f6ede19d0905f7
gabriel <gabriel.cordova@tecsup.edu.pe>
2021-10-15 15:34:25 -0500

    lab08

---
+++ lab08/series/views.py
@@ -0,0 +1,77 @@
+from django.views.decorators import csrf
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Serie
+from .serializers import SerieSerializer
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+
+
+# Create your views here.
+@api_view(['GET'])
+def index(request):
+    
+    
+    dataSeries = Serie.objects.all()
+    serializer = SerieSerializer(dataSeries,many=True)
+   
+    return Response(serializer.data)
+    
+@csrf_exempt
+def serie_list(request):
+    """
+    List all code serie, or create a new serie.
+    """
+    if request.method == 'GET':
+        series = Serie.objects.all()
+        serializer = SerieSerializer(series, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SerieSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+@csrf_exempt
+def serie_detail(request, pk):
+    
+    try:
+        serie = Serie.objects.get(pk=pk)
+    except Serie.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SerieSerializer(serie)
+        return JSONResponse(serializer.data)
+
+
+
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SerieSerializer(serie, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        serie.delete()
+        return HttpResponse(status=204)
+
